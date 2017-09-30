a28cbc3955b7fa37b46882dc7974d7ea59b9bbdd
steve0412 <tlstkdwls13@gmail.com>
2017-09-30 23:48:48 +0900

    .

---
+++ app/src/main/java/com/pakaking/sojun_final_project/LoginActivity.java
@@ -0,0 +1,17 @@
+package com.pakaking.sojun_final_project;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by tlstk on 2017-09-30.
+ */
+
+public class LoginActivity extends AppCompatActivity{
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
--- app/src/main/java/com/pakaking/sojun_final_project/MainActivity.java
+++ app/src/main/java/com/pakaking/sojun_final_project/MainActivity.java
@@ -1,13 +1,23 @@
 package com.pakaking.sojun_final_project;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.login);
+
+
+    }
+
+    public void loginClicked(View v) {
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(intent);
+        return;
     }
 }
